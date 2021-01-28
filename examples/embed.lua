e53fa1c7182f0242d86700d932f35b89e0d5c46c
Luiggy <44353442+YesLuiggy@users.noreply.github.com>
2021-01-28 18:43:37 -0300

    Update embed.lua

--- examples/embed.lua
+++ examples/embed.lua
@@ -1,5 +1,7 @@
 local API = require(game.ServerScriptService.HttpWrapper)
 
+print(API.mVersion)
+
 local Embed = API.Embed
 Embed.Title = "Hello, world!"
 Embed.Description = "Cool description!"
@@ -8,6 +10,7 @@ Embed.Thumbnail = "https://tr.rbxcdn.com/e338983714189a2e248f76170ee8f7df/150/15
 Embed.Image = "https://cdn.mos.cms.futurecdn.net/ntFmJUZ8tw3ULD3tkBaAtf.jpg"
 Embed.Footer = {text = "luiggy1200", icon_url = "https://tr.rbxcdn.com/e338983714189a2e248f76170ee8f7df/150/150/AvatarHeadshot/Png"}
 Embed.Url = "https://roblox.com"
+Embed.Fields = {{name = "Cool", value = "Value goes here!", inline = true}, {name = "Hi!", value = "Cool value.", inline = true}}
 Embed.Author = "well, author"
 
-API:Send("DiscordWebhookLink", Embed)
+API:Send("DISCORD_WEBHOOK_LINK", Embed)
