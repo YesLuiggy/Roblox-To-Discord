709a27fa0e53fb56426f3ed0096303579b596b41
Luiggy <44353442+YesLuiggy@users.noreply.github.com>
2021-01-28 13:06:36 -0300

    Added most of the to-do stuff.
    Enjoy

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 local HTTPService = game:GetService("HttpService")
 
 local module = {
-	mVersion = "1 Early Release", 
-	Embed = {Title = "", Description = "", Color = 0}
+	mVersion = "1 stable", 
+	Embed = {Title = "", Description = "", Color = 0, Image = "", Thumbnail = "", Url = "", Footer = ""}
 }
 
 function module:Send(Url, Content)
@@ -10,20 +10,46 @@ function module:Send(Url, Content)
 		return error("Url or Content is nil.")
 	end
 
-	local PayLoad
+	local PayLoad = {
+		["embeds"] = {{
+			
+		}}
+	}
 
 	if typeof(Content) == "table" then
-		if Content.Title and Content.Description and Content.Color then
-			PayLoad = {
-				["embeds"] = {{
-					["title"] = tostring(Content.Title),
-					["description"] = tostring(Content.Description),
-					["color"] = tonumber(Content.Color)
-				}}
-			}
+		if Content.Title then
+			PayLoad.embeds[1].title = tostring(Content.Title)
+		end
+
+		if Content.Description then
+			PayLoad.embeds[1].description = tostring(Content.Description)
+		end
+		
+		if Content.Color then
+			PayLoad.embeds[1].color = tonumber(Content.Color)
+		end
+		
+		if Content.Image then
+			PayLoad.embeds[1].image = {url = tostring(Content.Image)}
+		end
+		
+		if Content.Thumbnail then
+			PayLoad.embeds[1].thumbnail = {url = tostring(Content.Thumbnail)}
+		end
+		
+		if Content.Footer then
+			PayLoad.embeds[1].footer = {text = tostring(Content.Footer)}
+		end
+		
+		if Content.Url then
+			PayLoad.embeds[1].url = tostring(Content.Url)
+		end
+		
+		if Content.Author then
+			PayLoad.embeds[1].author = {text = tostring(Content.Author)}
 		end
 	end
-
+	
 	HTTPService:PostAsync(Url, HTTPService:JSONEncode(PayLoad))
 end
 
