d76b9cb376c91db5feca1a2da4cc1a48059a306a
Luiggy <44353442+YesLuiggy@users.noreply.github.com>
2021-01-27 22:03:09 -0300

    Main file added.

---
+++ main.lua
@@ -0,0 +1,30 @@
+local HTTPService = game:GetService("HttpService")
+
+local module = {
+	APIVersion = "1 Early Release", 
+	Embed = {Title = "", Description = "", Color = 0}
+}
+
+function module:Send(Url, Content)
+	if Url == nil or Content == nil then
+		return error("Url or Content is nil.")
+	end
+
+	local PayLoad
+
+	if typeof(Content) == "table" then
+		if Content.Title and Content.Description and Content.Color then
+			PayLoad = {
+				["embeds"] = {{
+					["title"] = tostring(Content.Title),
+					["description"] = tostring(Content.Description),
+					["color"] = tonumber(Content.Color)
+				}}
+			}
+		end
+	end
+
+	HTTPService:PostAsync(Url, HTTPService:JSONEncode(PayLoad))
+end
+
+return module
