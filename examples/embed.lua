2e5a2362b05ae667e68a10b0158866488984ffa6
Luiggy <44353442+YesLuiggy@users.noreply.github.com>
2021-01-29 20:09:52 -0300

    Update embed.lua

--- examples/embed.lua
+++ examples/embed.lua
@@ -1,16 +1,15 @@
 local API = require(game.ServerScriptService.HttpWrapper)
 
-print(API.mVersion)
-
 local Embed = API.Embed
 Embed.Title = "Hello, world!"
 Embed.Description = "Cool description!"
-Embed.Color = 3408005
-Embed.Thumbnail = "https://tr.rbxcdn.com/e338983714189a2e248f76170ee8f7df/150/150/AvatarHeadshot/Png"
-Embed.Image = "https://cdn.mos.cms.futurecdn.net/ntFmJUZ8tw3ULD3tkBaAtf.jpg"
-Embed.Footer = {text = "luiggy1200", icon_url = "https://tr.rbxcdn.com/e338983714189a2e248f76170ee8f7df/150/150/AvatarHeadshot/Png"}
 Embed.Url = "https://roblox.com"
+Embed.Timestamp = true
+Embed.Color = 3408005
+Embed.Footer = {text = "luiggy1200", icon_url = "https://tr.rbxcdn.com/e338983714189a2e248f76170ee8f7df/150/150/AvatarHeadshot/Png"}
+Embed.Image = {url = "https://cdn.pixabay.com/photo/2016/05/24/16/48/mountains-1412683__340.png"}
+Embed.Thumbnail = {url = "https://tr.rbxcdn.com/e338983714189a2e248f76170ee8f7df/150/150/AvatarHeadshot/Png"}
+Embed.Author = {name = "redacted", url = "https://devforum.roblox.com/u/luiggy1200/summary", icon_url = "https://tr.rbxcdn.com/e338983714189a2e248f76170ee8f7df/150/150/AvatarHeadshot/Png"}
 Embed.Fields = {{name = "Cool", value = "Value goes here!", inline = true}, {name = "Hi!", value = "Cool value.", inline = true}}
-Embed.Author = "well, author"
 
-API:Send("DISCORD_WEBHOOK_LINK", Embed)
+API:Send("DISCORD_WEBHOOK_URL", Embed)
