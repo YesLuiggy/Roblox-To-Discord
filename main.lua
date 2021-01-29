7c651f394969236264a7edb2268c41b7d1a3c189
Luiggy <44353442+YesLuiggy@users.noreply.github.com>
2021-01-28 22:33:19 -0300

    Fixed the author and removed some useless code.

--- main.lua
+++ main.lua
@@ -6,10 +6,11 @@ local module = {
 		Title = "", 
 		Description = "", 
 		Color = 0, 
+		Author = {name = "", url = "", icon_url = ""},
 		Image = "", 
 		Thumbnail = "", 
 		Url = "", 
-		Footer = {}, 
+		Footer = {text = "", icon_url = ""}, 
 		Fields = {name = "", value = "", inline = false}
 	}
 }
@@ -21,7 +22,7 @@ function module:Send(Url, Content)
 
 	local PayLoad = {
 		["embeds"] = {{
-			
+
 		}}
 	}
 
@@ -33,40 +34,36 @@ function module:Send(Url, Content)
 		if Content.Description then
 			PayLoad.embeds[1].description = tostring(Content.Description)
 		end
-		
+
 		if Content.Color then
 			PayLoad.embeds[1].color = tonumber(Content.Color)
 		end
-		
+
 		if Content.Image then
 			PayLoad.embeds[1].image = {url = tostring(Content.Image)}
 		end
-		
+
 		if Content.Thumbnail then
 			PayLoad.embeds[1].thumbnail = {url = tostring(Content.Thumbnail)}
 		end
-		
+
 		if Content.Footer then
-			if Content.Footer.icon_url then
-				PayLoad.embeds[1].footer = {text = Content.Footer.text, icon_url = Content.Footer.icon_url}
-			else
-				PayLoad.embeds[1].footer = {text = Content.Footer.text}
-			end
+			PayLoad.embeds[1].footer = Content.Footer
 		end
-		
+
 		if Content.Url then
 			PayLoad.embeds[1].url = tostring(Content.Url)
 		end
-		
+
 		if Content.Fields then
 			PayLoad.embeds[1].fields = Content.Fields
 		end
-		
+
 		if Content.Author then
-			PayLoad.embeds[1].author = {text = tostring(Content.Author)}
+			PayLoad.embeds[1].author = Content.Author
 		end
 	end
-	
+
 	HTTPService:PostAsync(Url, HTTPService:JSONEncode(PayLoad))
 end
 
