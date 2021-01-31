b8e5e4c99ea8aaf322a001e563944141c8cb3b0b
Luiggy <44353442+YesLuiggy@users.noreply.github.com>
2021-01-30 22:20:54 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ local HTTPService = game:GetService("HttpService")
 
 local module = {
 	mVersion = "v1.0 Stable", 
-	Embed = {
+	DiscordEmbed = {
 		Title = nil,	  -- string
 		Description = nil,-- string
 		Url = nil, 		  -- string
@@ -17,68 +17,66 @@ local module = {
 }
 
 function module:Send(Url, Content)
-	if Url == nil or Content == nil then
-		return error("Url or Content is nil.")
+	if Url == nil then
+		return error("Url is nil.")
 	end
+	
+	HTTPService:PostAsync(Url, HTTPService:JSONEncode(Content))
+end
 
-	local PayLoad = {
-		content = Content
+function module.DiscordEmbed:Build(Content)
+	local Embed = {
+		["embeds"] = {{
+
+		}}
 	}
+	
+	if Content.Title ~= nil then
+		Embed.embeds[1].title = Content.Title
+	end
 
-	if typeof(Content) == "table" then
-		PayLoad = {
-			["embeds"] = {{
+	if Content.Description ~= nil then
+		Embed.embeds[1].description = Content.Description
+	end
 
-			}}
-		}
+	if Content.Url ~= nil then
+		Embed.embeds[1].url = Content.Url
+	end
 
-		if Content.Title ~= nil then
-			PayLoad.embeds[1].title = Content.Title
-		end
+	if Content.Timestamp ~= nil then
+		if Content.Timestamp == true then
+			local OsDate = os.date("*t")
+			local Date = OsDate.year .. "-" .. OsDate.month .. "-" .. OsDate.day .. "T" .. OsDate.hour .. ":" .. OsDate.min .. ":" .. OsDate.sec .. "Z"
 
-		if Content.Description ~= nil then
-			PayLoad.embeds[1].description = Content.Description
-		end
-
-		if Content.Url ~= nil then
-			PayLoad.embeds[1].url = Content.Url
-		end
-
-		if Content.Timestamp ~= nil then
-			if Content.Timestamp == true then
-				local OsDate = os.date("*t")
-				local Date = OsDate.year .. "-" .. OsDate.month .. "-" .. OsDate.day .. "T" .. OsDate.hour .. ":" .. OsDate.min .. ":" .. OsDate.sec .. "Z"
-				
-				PayLoad.embeds[1].timestamp = tostring(Date)
-			end
-		end
-
-		if Content.Color ~= nil then
-			PayLoad.embeds[1].color = Content.Color
-		end
-
-		if Content.Footer ~= nil then
-			PayLoad.embeds[1].footer = Content.Footer
-		end
-
-		if Content.Image ~= nil then
-			PayLoad.embeds[1].image = Content.Image
-		end
-
-		if Content.Thumbnail ~= nil then
-			PayLoad.embeds[1].thumbnail = Content.Thumbnail
-		end
-
-		if Content.Author ~= nil then
-			PayLoad.embeds[1].author = Content.Author
-		end
-
-		if Content.Fields ~= nil then
-			PayLoad.embeds[1].fields = Content.Fields
+			Embed.embeds[1].timestamp = tostring(Date)
 		end
 	end
 
-	HTTPService:PostAsync(Url, HTTPService:JSONEncode(PayLoad))
+	if Content.Color ~= nil then
+		Embed.embeds[1].color = Content.Color
+	end
+
+	if Content.Footer ~= nil then
+		Embed.embeds[1].footer = Content.Footer
+	end
+
+	if Content.Image ~= nil then
+		Embed.embeds[1].image = Content.Image
+	end
+
+	if Content.Thumbnail ~= nil then
+		Embed.embeds[1].thumbnail = Content.Thumbnail
+	end
+
+	if Content.Author ~= nil then
+		Embed.embeds[1].author = Content.Author
+	end
+
+	if Content.Fields ~= nil then
+		Embed.embeds[1].fields = Content.Fields
+	end
+
+	return Embed
 end
 
 return module
